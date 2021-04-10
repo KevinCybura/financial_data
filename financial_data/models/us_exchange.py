ea71f78ef6ad9815bf8944ac2803746acf2a9866
KevinCybura <kevincybura@gmail.com>
2021-04-10 17:19:08 -0400

    Alembic

---
+++ financial_data/models/us_exchange.py
@@ -0,0 +1,21 @@
+from sqlalchemy.sql.schema import Column
+from sqlalchemy.sql.sqltypes import Integer
+from sqlalchemy.sql.sqltypes import String
+
+from financial_data.base import Base
+
+
+class UsExchange(Base):
+    __tablename__ = "us_exchange"
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    long_name = Column(String)
+    mic = Column(String)
+    tape_id = Column(String)
+    oats_id = Column(String)
+    ref_id = Column(String)
+    type = Column(String)
+
+    def __repr__(self):
+        return f"<UsExchange(name={self.name}, long_name={self.long_name}, id={self.id})>"
